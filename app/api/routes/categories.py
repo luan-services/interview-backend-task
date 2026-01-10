eac177ab451ffb9d01ed27b389a8e48b1faed621
luan-services <luandotservices@gmail.com>
2026-01-10 03:37:16 -0300

    feat: updated categories route to add code logic

--- app/api/routes/categories.py
+++ app/api/routes/categories.py
@@ -1,21 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select
+from typing import List
+from app.config.database import SessionDep
+from app.models.category import Category
 
 # https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-apirouter
 
 router = APIRouter(prefix="/categories", tags=["categories"])
 
-@router.get("/")
-async def read_categories():
-    return {"category" : "get endpoint"}
+@router.get("/", response_model=List[Category])
+def read_categories(session: SessionDep):
+    return session.exec(select(Category)).all()
 
-@router.post("/")
-async def create_category():
-    return {"category" : "create endpoint"}
+@router.post("/", response_model=Category)
+async def create_category(category: Category, session: SessionDep):
+    session.add(category)
+    session.commit()
+    session.refresh(category)
+    return category
 
-@router.put("/{category_id}")
-async def create_category():
-    return {"category" : "update endpoint"}
+@router.put("/{category_id}", response_model=Category)
+async def update_category(category_id: str, category_data: Category, session: SessionDep):
+    category = session.get(Category, category_id)
+    if not category:
+        raise HTTPException(status_code=404, detail="Category Not Found")
+    
+    input_data = category_data.model_dump(exclude_unset=True)
+    category.sqlmodel_update(input_data)
+
+    session.add(category)
+    session.commit()
+    session.refresh(category)
+    return category
 
 @router.delete("/{category_id}")
-async def delete_category():
-    return {"category" : "delete endpoint"}
+async def delete_category(category_id: int, session: SessionDep):
+    category = session.get(Category, category_id)
+    if not category:
+        raise HTTPException(status_code=404, detail="Categoria Not Found")
+    session.delete(category)
+    session.commit()
+    return {"message": "Category Deleted"}
--- app/models/category.py
+++ app/models/category.py
@@ -7,6 +7,8 @@ from typing import List, TYPE_CHECKING
 if TYPE_CHECKING:
     from .product import Product
 
+# class CategoryBase(SQLModel): this would be only needed if we would like to prevent the user to set the category ids
+
 # SQLModel defines this class as a data model (like an interface in Javascript), and table=True ensures this is ALSO a table model to be created on the database
 class Category(SQLModel, table=True):
     # setting default=None here lets the user set a custom id when creating a category (like 5442), or sending it as None, when doing
@@ -15,3 +17,7 @@ class Category(SQLModel, table=True):
     name: str
     # we must set the relationship here, this does not create a table, it only works as a helper if you want to do category.products
     products: List["Product"] = Relationship(back_populates="category", cascade_delete=True)
+    
+
+# class CategoryCreate(CategoryBase):
+# class CategoryUpdate(CategoryBase): again only needed if we would like to prevent the user to set the category ids
