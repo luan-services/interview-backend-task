06d07a3aa24f6505ffd57a02a61d0bf82227f4d2
luan-services <luandotservices@gmail.com>
2026-01-10 01:59:21 -0300

    fix: updated main.py to correctly import all models

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from app.config.database import create_db_and_tables
 
 # https://fastapi.tiangolo.com/advanced/events/#lifespan
 
-# import models here later
+from app.models.category import Category
+from app.models.product import Product
+from app.models.sale import Sale
 
 """ a context manager in Python is something that you can use in a with statement, the first part of the function, before the 
 yield, will be executed before the application starts. and the part after the yield will be executed after the application has finished. """
